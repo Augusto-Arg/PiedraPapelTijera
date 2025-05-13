5eb3a883c71d295031bc8e7094b83e3048dd4b52
Bond <augustobond950@gmail.com>
2025-05-13 15:10:02 -0300

    funcion que realiza objeto aleatorio

--- piedrapapeltijera.js
+++ piedrapapeltijera.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+  const randomNum = Math.random();
+
+  if (randomNum < 0.33) {
+    return "piedra";
+  } else if (randomNum < 0.66) {
+    return "papel";
+  } else {
+    return "tijeras";
+  }
+}
+console.log(getComputerChoice())
